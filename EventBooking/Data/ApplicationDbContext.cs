e0374ff30ea2c193f9dc7743e25a49decaa3a785
Najathmhd <najamhd037@gmail.com>
2026-02-10 09:22:47 +0530

    data file created

---
+++ EventBooking/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using EventBooking.Models;
+
+namespace EventBooking.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventCategory> EventCategories { get; set; }
+        public DbSet<Venue> Venues { get; set; }
+    }
+}
